a83b52e41bc0716889080db8cba79cf8eb9f9c27
Narunat <n.pantapalin@tu-braunschweig.de>
2026-01-29 15:31:55 +0100

    update backup

--- run_train.sh
+++ run_train.sh
@@ -5,6 +5,8 @@
 #SBATCH --job-name=ma_unsup_train
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:ampere
+#SBATCH --mail-user=n.pantapalin@tu-braunschweig.de
+#SBATCH --mail-type=END,FAIL
 
 
 singularity exec --nv /home/y0113799/container/ma.sif python -u main_SEDFSVGP.py
